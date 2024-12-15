b7ea35c96f54bfecebd3e10361795f6d7d95ee8f
dipto230 <diptoroy230305@gmail.com>
2024-12-16 01:48:00 +0600

    job detail add with id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
@@ -30,7 +30,22 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
+   //jobs related APIS
+   const jobsCollection = client.db('jobPortal').collection('jobs');
+   app.get('/jobs',async(req, res)=>{
+    const cursor = jobsCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+   })
 
+   app.get('/jobs/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query  = {_id: new ObjectId(id)}
+    const result = await jobsCollection.findOne(query);
+    res.send(result);
+     
+
+   })
 
 
 
